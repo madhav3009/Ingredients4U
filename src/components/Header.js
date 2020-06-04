15cf366dc300d12e51e6b77a7e300a3f060c4bd2
Madhav <36961049+madhav3009@users.noreply.github.com>
2020-06-05 01:44:54 +0530

    c

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ const Header = props => {
   const { search, onInputChange, onSearchClick } = props;
   return (
     <div className="jumbotron">
-      <h1 className="display-1">
+      <h1 className="display-3">
         <i class="material-icons brand-icon ">fastfood</i> Search Your Recipe
-        <br /><h3 className="display-5">You Name The Food , We Give you The Recipe</h3><br />
+        <br /><h3 className="display-5">You Name The Food , We Give you The Recipe</h3>
       </h1>
-      <div class="input-group w-50 mx-auto">
+      <div class="input-group w-30 mx-auto p-4">
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Enter Your Recipe..."
           value={search}
           onChange={onInputChange}
